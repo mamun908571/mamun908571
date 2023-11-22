f41eb99814b9128b0630ccf43e840a35e58cec16
mamun908571 <mdmamun908571@gmail.com>
2023-11-23 01:15:34 +0600

    dictionary files added

--- dictionary.py
+++ dictionary.py
@@ -19,3 +19,12 @@
 car = {"model":"ml","color":"red","price":100}
 for i,j in car.items():
     print(i,j)
+
+
+cars = {"car1":
+        {"model":'m1',"price":100},
+        "car2":
+        {"model":'m2',"price":1000}}
+for i,j in cars.items():
+    for k in j.items():
+        print(i,k)
